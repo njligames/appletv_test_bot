b84bf831abb3f803854f1c4c4f5103acb1923d0c
njligames <jamesfolk1@gmail.com>
2018-03-26 23:46:22 -0400

    first

---
+++ NJLIC.build/Debug-appletvsimulator/ALL_BUILD.build/Script-0884A3EC06AA43ECBB33645B.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/jamesfolk/Documents/NJLI/TESTBED/test_project_repo/_build_appletv -f /Users/jamesfolk/Documents/NJLI/TESTBED/test_project_repo/_build_appletv/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ NJLIC.build/Debug-appletvsimulator/NJLIC.build/Script-DB84CC0F85044C46915E5E82.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/jamesfolk/Documents/NJLI/TESTBED/test_project_repo/_build_appletv -f /Users/jamesfolk/Documents/NJLI/TESTBED/test_project_repo/_build_appletv/CMakeScripts/NJLIC_postBuildPhase.make$CONFIGURATION all
